10d8c49c463e6e6fc6da484ff843a404d707f83d
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-02-25 16:23:31 -0600

    changes to main.js

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,13 @@
 const Main = () => {
     return(
-        <main class="mainstuff">Hello there, this is finally fucking working. Nice!</main>
+        <main class="mainstuff">
+            <h2>Main Section Title Spot....</h2>
+            <p>Hello there, this is finally fucking working. Nice! </p>
+            <p>Hello there, this is finally fucking working. Nice!</p>
+            <p>Hello there, this is finally fucking working. Nice!</p>
+            <p>Hello there, this is finally fucking working. Nice!</p>
+            <p>Hello there, this is finally fucking working. Nice!</p>
+            </main>
     )
 }
 
